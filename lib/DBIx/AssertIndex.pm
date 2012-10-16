9c659138c19b274c3d38d2e6b3897416af36f9c7
Shigeki Morimoto <github@morimo.to>
2012-10-16 13:31:42 +0900

    initial commit

---
+++ lib/DBIx/AssertIndex.pm
@@ -0,0 +1,32 @@
+package DBIx::AssertIndex;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::AssertIndex -
+
+=head1 SYNOPSIS
+
+  use DBIx::AssertIndex;
+
+=head1 DESCRIPTION
+
+DBIx::AssertIndex is
+
+=head1 AUTHOR
+
+mixi-inc E<lt>shigeki.morimoto@mixi.co.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
